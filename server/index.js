0fece1a2e1f0cf2e0dbd2c385572b28645e44b49
Abi <abigail.schmucker@gmail.com>
2018-02-25 20:48:21 -0600

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'site')));
+
+const server = app.listen(8080, () => {
+	console.log('Server listening on port 8080');
+});
