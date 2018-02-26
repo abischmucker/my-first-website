e8ee48eca5a13f88dc1c570b5983a8d4f20bd997
Jesse <jesselee34@gmail.com>
2018-02-25 21:36:33 -0600

    Fixed the static server

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'site')));
+app.use(express.static(path.join(__dirname, '../site')));
 
 const server = app.listen(8080, () => {
 	console.log('Server listening on port 8080');
